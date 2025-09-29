f472a7516fd99bbf43f83c7a230cec80b039faff
amifistore <storeamifi@gmail.com>
2025-09-29 19:54:18 +0700

    Update produk_daftar_handler.py

--- handlers/produk_daftar_handler.py
+++ handlers/produk_daftar_handler.py
@@ -1,9 +1,9 @@
-from produk import get_produk_list
 from markup import produk_inline_keyboard
+from produk import get_produk_list
 
 def lihat_produk_callback(update, context):
     produk_list = get_produk_list()
     update.message.reply_text(
-        "🛒 Pilih produk yang ingin dipesan:",
+        "🛒 Pilih produk yang ingin dibeli:",
         reply_markup=produk_inline_keyboard(produk_list)
     )
